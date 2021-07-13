b94c82de476d25471c77c6ffc1a1a358bc0f6c90
vgoylo <y_draiv.by@mail.ru>
2021-07-13 16:52:15 +0300

    Initial commit

--- test_hash.rb
+++ test_hash.rb
@@ -1,12 +1,10 @@
+
 def common_keys(hash1,hash2)
   keys1 = hash1.keys
   keys2 = hash2.keys 
   
-
   keys1 & keys2
 end
-
-
 
 p common_keys({
   "The Batman" => "Bruce Wayne",
@@ -17,15 +15,3 @@ p common_keys({
   "Superman" => "Clark Kent",
   "Wonder Woman" => "Diana Prince"
 })
-
-
-
-
-# hash.each do |key,value| 
-#   if key.to_s.include?('a') && value.to_s.include?('1')
-#     result[key] = value
-#   end
-# end
-
-# result
-# end
